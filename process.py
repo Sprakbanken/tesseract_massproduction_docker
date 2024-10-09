0d9cd053d6da190a07c8aa17a9562cd68f17829b
Magnus Breder Birkenes <magnus.birkenes@nb.no>
2024-10-09 15:19:12 +0200

    do not create folder if it exists; fix default output

--- process.py
+++ process.py
@@ -13,25 +13,24 @@ except:
 try:
     output = sys.argv[3]
 except:
-    output = 'alto'
+    output = ''
 
 manifest_url = "https://api.nb.no/catalog/v1/iiif/%s/manifest" % (urn)
 manifest = requests.get(manifest_url)
 manifest_json = manifest.json()
 
 folder_path = os.path.join('/data', urn)
-os.mkdir(folder_path)
 
-if manifest_json["license"] in ["https://www.nb.no/lisens/#public-domain", "https://www.nb.no/lisens/stromming"]:
-    for idx,canvas in enumerate(manifest_json["sequences"][0]["canvases"]):
-        image_id = canvas["images"][0]["@id"].split('/')[-1]
-        image_url = canvas["images"][0]["resource"]["@id"]
-        out_path = os.path.join(folder_path, image_id)
-        image_url = image_url.replace('/full/full/', '/full/pct:50/')
-        print(image_url)
-        if output == "alto":
-            subprocess.run(["tesseract", image_url, out_path, "-l", model, "--dpi", "400", "-c", "tessedit_create_alto=1", "-c", "preserve_interword_spaces=1"], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
-        else:
-            subprocess.run(["tesseract", image_url, 'stdout', "-l", model, "--dpi", "400"])
-else:
-    print("%s not in the public domain, pipeline will not work." % (urn))
+if not os.path.exists(folder_path):
+    os.mkdir(folder_path)
+
+for idx,canvas in enumerate(manifest_json["sequences"][0]["canvases"]):
+    image_id = canvas["images"][0]["@id"].split('/')[-1]
+    image_url = canvas["images"][0]["resource"]["@id"]
+    out_path = os.path.join(folder_path, image_id)
+    image_url = image_url.replace('/full/full/', '/full/pct:50/')
+    print(image_url)
+    if output == "alto":
+        subprocess.run(["tesseract", image_url, out_path, "-l", model, "--dpi", "400", "-c", "tessedit_create_alto=1", "-c", "preserve_interword_spaces=1"], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+    else:
+        subprocess.run(["tesseract", image_url, 'stdout', "-l", model, "--dpi", "400"])
